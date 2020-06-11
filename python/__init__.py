b815966161b186810623cb15b988649093d1ad15
Ville Kuosmanen <vibe96@gmail.com>
2020-06-11 21:57:36 +0300

    Delete unused files. (#43)

--- python/__init__.py
+++ python/__init__.py
@@ -1,7 +1,6 @@
 from .f1Data import *
 from .dataclean import *
 from .f1Models import *
-from .simulation import *
 from .datasetBuilding import *
 from .monteCarlo import *
 from .baselineModels import *
