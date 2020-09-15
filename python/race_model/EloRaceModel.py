d9a0e0d651ae2fb52627fa6ff746911f5dec02c6
Ville Kuosmanen <vibe96@gmail.com>
2020-09-15 19:36:47 +0300

    Small bug fixes on race model track ratings

--- python/race_model/EloRaceModel.py
+++ python/race_model/EloRaceModel.py
@@ -1,3 +1,4 @@
+from python.f1Models import Engine, Constructor
 import statistics
 import math
 
@@ -13,7 +14,6 @@ CONSTRUCTOR_WEIGHTING = 0.69
 ENGINE_WEIGHTING = 0.09
 TRACK_WEIGHTING = 0.03
 
-from python.f1Models import Engine, Constructor
 
 class EloDriver:
     def __init__(self, name, constructor):
@@ -35,16 +35,23 @@ class EloRaceModel:
         self.tracks = tracks
 
     def getGaElo(self, driverId, gridPosition, trackId):
-        gridAdjustment = self.tracks[trackId] * self.getGridAdjustment(gridPosition)
+        gridAdjustment = self.tracks[trackId] * \
+            self.getGridAdjustment(gridPosition)
 
         return (self.drivers[driverId].rating * DRIVER_WEIGHTING) + \
             (self.drivers[driverId].constructor.rating * CONSTRUCTOR_WEIGHTING) + \
-            (self.drivers[driverId].constructor.engine.rating * ENGINE_WEIGHTING) + gridAdjustment + GA_ELO_INTERCEPT_COEFFICIENT
+            (self.drivers[driverId].constructor.engine.rating *
+             ENGINE_WEIGHTING) + gridAdjustment + GA_ELO_INTERCEPT_COEFFICIENT
 
     def getGaEloWithTrackAlpha(self, driverId, gridPosition, trackId, alphaAdjustment):
-        gridAdjustment = (self.tracks[trackId] * alphaAdjustment) * self.getGridAdjustment(gridPosition)
+        gridAdjustment = (
+            self.tracks[trackId] * alphaAdjustment) * self.getGridAdjustment(gridPosition)
 
-        return (self.drivers[driverId].rating)*DRIVER_WEIGHTING + (self.drivers[driverId].constructor.rating)*CONSTRUCTOR_WEIGHTING + (self.drivers[driverId].constructor.engine.rating)*ENGINE_WEIGHTING + (self.drivers[driverId].trackRatings[trackId]*TRACK_WEIGHTING) + gridAdjustment + GA_ELO_INTERCEPT_COEFFICIENT
+        return (self.drivers[driverId].rating)*DRIVER_WEIGHTING + \
+            (self.drivers[driverId].constructor.rating)*CONSTRUCTOR_WEIGHTING + \
+            (self.drivers[driverId].constructor.engine.rating)*ENGINE_WEIGHTING + \
+            (self.drivers[driverId].trackRatings[trackId]*TRACK_WEIGHTING) + \
+            gridAdjustment + GA_ELO_INTERCEPT_COEFFICIENT
 
     def getGridAdjustment(self, gridPosition):
         return (9.5 - gridPosition) * GRID_ADJUSTMENT_COEFFICIENT
@@ -61,18 +68,8 @@ class EloRaceModel:
         constructor.rating += (adjustment * K_FACTOR)
 
     def adjustEloRatingEngine(self, engine, adjustment):
-        engine.rating += (adjustment * K_FACTOR)  # TODO check if this is correct
-
-    def _addNewTracksToEntities(self, driverId, trackId):
-        if trackId not in self.drivers[driverId].trackRatings:
-            # TODO maybe change defaults
-            self.drivers[driverId].trackRatings[trackId] = 2200
-        if trackId not in self.drivers[driverId].constructor.trackRatings:
-            # TODO maybe change defaults
-            self.drivers[driverId].constructor.trackRatings[trackId] = 2200
-        if trackId not in self.drivers[driverId].constructor.engine.trackRatings:
-            # TODO maybe change defaults
-            self.drivers[driverId].constructor.engine.trackRatings[trackId] = 2200
+        # TODO check if this is correct
+        engine.rating += (adjustment * K_FACTOR)
 
     def adjustCircuitAplha(self, alphaAdjustment, trackId):
         self.tracks[trackId] *= alphaAdjustment
@@ -80,19 +77,22 @@ class EloRaceModel:
     def addNewTrack(self, circuitId):
         if circuitId not in self.tracks:
             self.tracks[circuitId] = 1
-          
+
+
 class EloConstructor:
     def __init__(self, name, engine):
         self.name = name
         self.engine = engine
-        self.rating = 2200 # Default rating
+        self.rating = 2200  # Default rating
         self.trackRatings = {}
+
 
 class EloEngine:
     def __init__(self, name):
         self.name = name
         self.trackRatings = {}
         self.rating = 2200  # Default rating
+
 
 class EloRaceModelGenerator:
     def __init__(self, seasonsData, raceResultsData, driversData, constructorsData, enginesData):
@@ -121,10 +121,11 @@ class EloRaceModelGenerator:
                     resultsForRace = self.raceResultsData[raceId]
                     self._addNewDriversAndConstructors(resultsForRace, year)
                     self.model.addNewTrack(data.circuitId)
-                    self._addNewTracksToEntities(data.trackId)
 
                     results = {}
                     for index, res in enumerate(resultsForRace):
+                        self._addNewTracksToEntities(
+                            res["driverId"], data.circuitId)
                         results[res["driverId"]] = res["position"]
                     driverIds = [x["driverId"] for x in resultsForRace]
                     eloAdjustments, alphaAdjustment = self._calculateTrackAlphaAdjustmentAndBestEloAdjustments(
@@ -133,9 +134,9 @@ class EloRaceModelGenerator:
                     for driverId in driverIds:
                         if results[driverId] is None:
                             self.model.adjustEloRating(
-                                driverId, RETIREMENT_PENALTY)
+                                driverId, RETIREMENT_PENALTY, data.circuitId)
                         self.model.adjustEloRating(
-                            driverId, eloAdjustments[0][driverId] + FINISHING_BONUS)
+                            driverId, eloAdjustments[0][driverId] + FINISHING_BONUS, data.circuitId)
 
                     for constructor in eloAdjustments[1]:
                         self.model.adjustEloRatingConstructor(
@@ -144,8 +145,9 @@ class EloRaceModelGenerator:
                     for engine in eloAdjustments[2]:
                         self.model.adjustEloRatingConstructor(
                             engine, eloAdjustments[2][engine])
-                    
-                    self.model.adjustCircuitAplha(alphaAdjustment, data.circuitId)
+
+                    self.model.adjustCircuitAplha(
+                        alphaAdjustment, data.circuitId)
 
     def generatePredictions(self):
         self.model = EloRaceModel({}, {}, {}, {})
@@ -165,6 +167,8 @@ class EloRaceModelGenerator:
                     results = {}
                     gaElos = {}
                     for index, res in enumerate(resultsForRace):
+                        self._addNewTracksToEntities(
+                            res["driverId"], data.circuitId)
                         results[res["driverId"]] = res["position"]
                         gaElos[res["driverId"]] = self.model.getGaElo(
                             res["driverId"], res["grid"], data.circuitId)
@@ -184,9 +188,9 @@ class EloRaceModelGenerator:
                     for driverId in driverIds:
                         if results[driverId] is None:
                             self.model.adjustEloRating(
-                                driverId, RETIREMENT_PENALTY)
+                                driverId, RETIREMENT_PENALTY, data.circuitId)
                         self.model.adjustEloRating(
-                            driverId, eloAdjustments[0][driverId] + FINISHING_BONUS)
+                            driverId, eloAdjustments[0][driverId] + FINISHING_BONUS, data.circuitId)
 
                     for constructor in eloAdjustments[1]:
                         self.model.adjustEloRatingConstructor(
@@ -195,8 +199,9 @@ class EloRaceModelGenerator:
                     for engine in eloAdjustments[2]:
                         self.model.adjustEloRatingConstructor(
                             engine, eloAdjustments[2][engine])
-                    
-                    self.model.adjustCircuitAplha(alphaAdjustment, data.circuitId)
+
+                    self.model.adjustCircuitAplha(
+                        alphaAdjustment, data.circuitId)
         return predictions
 
     def _updateModelsForYear(self, season):
@@ -225,14 +230,28 @@ class EloRaceModelGenerator:
         # Regress all powers towards the mean
         # TODO
 
+    def _addNewTracksToEntities(self, driverId, trackId):
+        if trackId not in self.model.drivers[driverId].trackRatings:
+            # TODO maybe change defaults
+            self.model.drivers[driverId].trackRatings[trackId] = 2200
+        if trackId not in self.model.drivers[driverId].constructor.trackRatings:
+            # TODO maybe change defaults
+            self.model.drivers[driverId].constructor.trackRatings[trackId] = 2200
+        if trackId not in self.model.drivers[driverId].constructor.engine.trackRatings:
+            # TODO maybe change defaults
+            self.model.drivers[driverId].constructor.engine.trackRatings[trackId] = 2200
+
     def _addNewDriversAndConstructors(self, resultsForRace, year):
         for res in resultsForRace:
             if res["driverId"] not in self.model.drivers:
-                self.model.drivers[res["driverId"]] = EloDriver(self.driversData[res["driverId"]], res["constructorId"])
+                self.model.drivers[res["driverId"]] = EloDriver(
+                    self.driversData[res["driverId"]], res["constructorId"])
                 if year > 2003:
-                    self.model.drivers[res["driverId"]].rating = ROOKIE_DRIVER_RATING
+                    self.model.drivers[res["driverId"]
+                                       ].rating = ROOKIE_DRIVER_RATING
             if self.model.drivers[res["driverId"]].constructor is not self.model.constructors[res["constructorId"]]:
-                self.model.drivers[res["driverId"]].constructor = self.model.constructors[res["constructorId"]]
+                self.model.drivers[res["driverId"]
+                                   ].constructor = self.model.constructors[res["constructorId"]]
 
     def _calculateEloAdjustments(self, driverIds, gaElos, results):
         driverAdjustments = {}
@@ -251,21 +270,31 @@ class EloRaceModelGenerator:
                     constructorAdjustments[self.model.drivers[driverIds[k]].constructor] = 0
 
                 if self.model.drivers[driverIds[i]].constructor.engine not in engineAdjustments:
-                    engineAdjustments[self.model.drivers[driverIds[i]].constructor.engine] = 0
+                    engineAdjustments[self.model.drivers[driverIds[i]
+                                                         ].constructor.engine] = 0
                 if self.model.drivers[driverIds[k]].constructor.engine not in engineAdjustments:
-                    engineAdjustments[self.model.drivers[driverIds[k]].constructor.engine] = 0
+                    engineAdjustments[self.model.drivers[driverIds[k]
+                                                         ].constructor.engine] = 0
 
                 if results[driverIds[i]] is not None and results[driverIds[k]] is not None:
-                    headToHeadResult = 1 if results[driverIds[i]] < results[driverIds[k]] else 0
-                    expectedScore = self.model.getExpectedScore(gaElos[driverIds[i]], gaElos[driverIds[k]])
-                    driverAdjustments[driverIds[i]] += headToHeadResult - expectedScore
-                    driverAdjustments[driverIds[k]] += expectedScore - headToHeadResult
+                    headToHeadResult = 1 if results[driverIds[i]
+                                                    ] < results[driverIds[k]] else 0
+                    expectedScore = self.model.getExpectedScore(
+                        gaElos[driverIds[i]], gaElos[driverIds[k]])
+                    driverAdjustments[driverIds[i]
+                                      ] += headToHeadResult - expectedScore
+                    driverAdjustments[driverIds[k]
+                                      ] += expectedScore - headToHeadResult
 
-                    constructorAdjustments[self.model.drivers[driverIds[i]].constructor] += headToHeadResult - expectedScore
-                    constructorAdjustments[self.model.drivers[driverIds[k]].constructor] += expectedScore - headToHeadResult
+                    constructorAdjustments[self.model.drivers[driverIds[i]
+                                                              ].constructor] += headToHeadResult - expectedScore
+                    constructorAdjustments[self.model.drivers[driverIds[k]
+                                                              ].constructor] += expectedScore - headToHeadResult
 
-                    engineAdjustments[self.model.drivers[driverIds[i]].constructor.engine] += headToHeadResult - expectedScore
-                    engineAdjustments[self.model.drivers[driverIds[k]].constructor.engine] += expectedScore - headToHeadResult
+                    engineAdjustments[self.model.drivers[driverIds[i]
+                                                         ].constructor.engine] += headToHeadResult - expectedScore
+                    engineAdjustments[self.model.drivers[driverIds[k]
+                                                         ].constructor.engine] += expectedScore - headToHeadResult
 
         return (driverAdjustments, constructorAdjustments, engineAdjustments)
 
@@ -273,7 +302,8 @@ class EloRaceModelGenerator:
         eloAdjustments = ()
         eloAdjustmentsSum = None
         bestAdjustment = 0
-        adjustments = [0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99, 1, 1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09, 1.1]
+        adjustments = [0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98,
+                       0.99, 1, 1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09, 1.1]
         for alphaAdjustment in adjustments:
             results = {}
             gaElos = {}
@@ -284,9 +314,12 @@ class EloRaceModelGenerator:
             curEloAdjustments = self._calculateEloAdjustments(
                 driverIds, gaElos, results)
             curEloAdjustmentsSum = 0
-            curEloAdjustmentsSum += statistics.mean(map(abs, curEloAdjustments[0].values()))
-            curEloAdjustmentsSum += statistics.mean(map(abs, curEloAdjustments[1].values()))
-            curEloAdjustmentsSum += statistics.mean(map(abs, curEloAdjustments[2].values()))
+            curEloAdjustmentsSum += statistics.mean(
+                map(abs, curEloAdjustments[0].values()))
+            curEloAdjustmentsSum += statistics.mean(
+                map(abs, curEloAdjustments[1].values()))
+            curEloAdjustmentsSum += statistics.mean(
+                map(abs, curEloAdjustments[2].values()))
 
             if not eloAdjustmentsSum or curEloAdjustmentsSum < eloAdjustmentsSum:
                 eloAdjustmentsSum = curEloAdjustmentsSum
