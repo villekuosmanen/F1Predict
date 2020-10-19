a03b6c967da3f5f73ae527f96c9ed1f9c6b0a2f6
Ville Kuosmanen <vibe96@gmail.com>
2020-10-19 20:45:08 +0300

    Optimise weights, plus other smaller fixes

--- f1predict/race/DataProcessor.py
+++ f1predict/race/DataProcessor.py
@@ -1,14 +1,13 @@
 import statistics
 
-import f1predict.race.utils as utils
 from f1predict.race.EloModel import EloModel, EloDriver, EloConstructor, EloEngine
 
-RETIREMENT_PENALTY = -0.5
+RETIREMENT_PENALTY = -1.8
 FINISHING_BONUS = 0.1
 BASE_RETIREMENT_PROBABILITY = 0.1
 RETIREMENT_PROBABILITY_CHANGE_TRACK = 0.33
 RETIREMENT_PROBABILITY_CHANGE_DRIVER = 0.10
-ROOKIE_DRIVER_RATING = 1800
+ROOKIE_DRIVER_RATING = 1820
 
 class DataProcessor:
     def __init__(self, seasonsData, raceResultsData, driversData, constructorsData, enginesData):
@@ -228,7 +227,7 @@ class DataProcessor:
                 eng_retirements[self.model.drivers[driverID].constructor.engine] = []
 
             all_retirements.append(1)
-            blame = utils.getRetirementBlame(retirementReason)
+            blame = getRetirementBlame(retirementReason)
             self.model.drivers[driverID].retirementProbability = (3 * blame[0] * RETIREMENT_PROBABILITY_CHANGE_DRIVER) + \
                 (1-RETIREMENT_PROBABILITY_CHANGE_DRIVER) * self.model.drivers[driverID].retirementProbability
             const_retirements[self.model.drivers[driverID].constructor].append(blame[1])
@@ -257,3 +256,84 @@ class DataProcessor:
             newValue = statistics.mean(blames)
             engine.retirementProbability = (3 * newValue * RETIREMENT_PROBABILITY_CHANGE_DRIVER) + \
                 (1-RETIREMENT_PROBABILITY_CHANGE_DRIVER) * engine.retirementProbability
+
+def getRetirementBlame(status):
+    if status == "Collision":
+        return (1, 0, 0)
+    elif status == "Accident":
+        return (1, 0, 0)
+    elif status == "Engine":
+        return (0, 0, 1)
+    elif status == "Gearbox":
+        return (0, 1, 0)
+    elif status == "Hydraulics":
+        return (0, 1, 0)
+    elif status == "Brakes":
+        return (0, 1, 0)
+    elif status == "Spun off":
+        return (1, 0, 0)
+    elif status == "Suspension":
+        return (0, 1, 0)
+    elif status == "Electrical":
+        return (0, 0.5, 0.5)
+    elif status == "Power Unit":
+        return (0, 0, 1)
+    elif status == "Collision damage":
+        return (1, 0, 0)
+    elif status == "Wheel":
+        return (0, 1, 0)
+    elif status == "Transmission":
+        return (0, 1, 0)
+    elif status == "Mechanical":
+        return (0, 0.5, 0.5)
+    elif status == "Puncture":
+        return (0.667, 0.333, 0)
+    elif status == "Driveshaft":
+        return (0, 1, 0)
+    elif status == "Oil leak":
+        return (0, 0.5, 0.5)
+    elif status == "Tyre":
+        return (0, 1, 0)
+    elif status == "Fuel pressure":
+        return (0, 0.5, 0.5)
+    elif status == "Clutch":
+        return (0, 1, 0)
+    elif status == "Electronics":
+        return (0, 0.5, 0.5)
+    elif status == "Power loss":
+        return (0, 0, 1)
+    elif status == "Overheating":
+        return (0, 0.5, 0.5)
+    elif status == "Throttle":
+        return (0, 1, 0)
+    elif status == "Wheel nut":
+        return (0, 1, 0)
+    elif status == "Exhaust":
+        return (0, 1, 0)
+    elif status == "Steering":
+        return (0, 1, 0)
+    elif status == "Fuel system":
+        return (0, 0.5, 0.5)
+    elif status == "Water leak":
+        return (0, 1, 0)
+    elif status == "Battery":
+        return (0, 0, 1)
+    elif status == "ERS":
+        return (0, 0, 1)
+    elif status == "Water pressure":
+        return (0, 1, 0)
+    elif status == "Rear wing":
+        return (0.667, 0.333, 0)
+    elif status == "Vibrations":
+        return (0.667, 0.333, 0)
+    elif status == "Technical":
+        return (0, 0.5, 0.5)
+    elif status == "Oil pressure":
+        return (0, 0.5, 0.5)
+    elif status == "Pneumatics":
+        return (0, 1, 0)
+    elif status == "Turbo":
+        return (0, 0, 1)
+    elif status == "Front wing":
+        return (0.667, 0.333, 0)
+    return (0.334, 0.333, 0.333)
--- f1predict/race/utils.py
+++ f1predict/race/utils.py
@@ -36,8 +36,7 @@ def generateModel():
     processor = DataProcessor(
         seasonsData, raceResultsData, driversData, constructorsData, enginesData)
     processor.processDataset()
-    raceModel = processor.getModel()
-    return raceModel
+    return processor.getModel()
 
 def overwriteRaceModelWithNewDrivers(raceModel):
     newDrivers = json.load(open('data/newDrivers.json'))["drivers"]
@@ -58,84 +57,3 @@ def calculateGaElos(raceModel, grid, circuit):
         gaElo = raceModel.getGaElo(did, gridPosition, circuit)
         gaElos.append((did, gaElo))
     return gaElos
-
-def getRetirementBlame(status):
-    if status == "Collision":
-        return (1, 0, 0)
-    elif status == "Accident":
-        return (1, 0, 0)
-    elif status == "Engine":
-        return (0, 0, 1)
-    elif status == "Gearbox":
-        return (0, 1, 0)
-    elif status == "Hydraulics":
-        return (0, 1, 0)
-    elif status == "Brakes":
-        return (0, 1, 0)
-    elif status == "Spun off":
-        return (1, 0, 0)
-    elif status == "Suspension":
-        return (0, 1, 0)
-    elif status == "Electrical":
-        return (0, 0.5, 0.5)
-    elif status == "Power Unit":
-        return (0, 0, 1)
-    elif status == "Collision damage":
-        return (1, 0, 0)
-    elif status == "Wheel":
-        return (0, 1, 0)
-    elif status == "Transmission":
-        return (0, 1, 0)
-    elif status == "Mechanical":
-        return (0, 0.5, 0.5)
-    elif status == "Puncture":
-        return (0.667, 0.333, 0)
-    elif status == "Driveshaft":
-        return (0, 1, 0)
-    elif status == "Oil leak":
-        return (0, 0.5, 0.5)
-    elif status == "Tyre":
-        return (0, 1, 0)
-    elif status == "Fuel pressure":
-        return (0, 0.5, 0.5)
-    elif status == "Clutch":
-        return (0, 1, 0)
-    elif status == "Electronics":
-        return (0, 0.5, 0.5)
-    elif status == "Power loss":
-        return (0, 0, 1)
-    elif status == "Overheating":
-        return (0, 0.5, 0.5)
-    elif status == "Throttle":
-        return (0, 1, 0)
-    elif status == "Wheel nut":
-        return (0, 1, 0)
-    elif status == "Exhaust":
-        return (0, 1, 0)
-    elif status == "Steering":
-        return (0, 1, 0)
-    elif status == "Fuel system":
-        return (0, 0.5, 0.5)
-    elif status == "Water leak":
-        return (0, 1, 0)
-    elif status == "Battery":
-        return (0, 0, 1)
-    elif status == "ERS":
-        return (0, 0, 1)
-    elif status == "Water pressure":
-        return (0, 1, 0)
-    elif status == "Rear wing":
-        return (0.667, 0.333, 0)
-    elif status == "Vibrations":
-        return (0.667, 0.333, 0)
-    elif status == "Technical":
-        return (0, 0.5, 0.5)
-    elif status == "Oil pressure":
-        return (0, 0.5, 0.5)
-    elif status == "Pneumatics":
-        return (0, 1, 0)
-    elif status == "Turbo":
-        return (0, 0, 1)
-    elif status == "Front wing":
-        return (0.667, 0.333, 0)
-    return (0.334, 0.333, 0.333)
